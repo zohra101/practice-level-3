89237f2881385ad30557e86af756da9e9eaf9e76
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-12-12 19:40:40 -0600

    More files

---
+++ utils/Row.js
@@ -0,0 +1,16 @@
+import { Column } from " /Column.js";
+
+export class Row {
+    openingTag = "‹div class='row'>";
+    closingTag = "</div>";
+    innerHTML = "";
+
+    addColumn (column = new Column()) {
+        this.innerHTML += column;
+    }
+
+    tostring() {
+        return `${this.openingTag}${this.innerHTML}${this.closingTag}`;
+    }
+
+}
